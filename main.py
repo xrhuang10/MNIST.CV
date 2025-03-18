9465ffbc6b708246edf19dd500e08c95572b38ac
xrhuang10 <xrhuang10@gmail.com>
2025-03-18 12:41:33 -0400

    eve

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ users = {
     1: {"id": 1, "name": "Alice"},
     2: {"id": 2, "name": "Bob"},
     3: {"id": 3, "name": "Charlie"},
-    4: {"id": 4, "name": "David"}
+    4: {"id": 4, "name": "David"},
+    5: {"id": 5, "name": "Eve"},
 }
 
 @app.route("/")
